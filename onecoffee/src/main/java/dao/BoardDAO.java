8fcbdad0bbaeb2db5b84f342f92378938b2909d5
YoonSeo <kky77209@gmail.com>
2023-11-28 17:49:10 +0900

    board 객체, dao

---
+++ onecoffee/src/main/java/dao/BoardDAO.java
@@ -0,0 +1,57 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import db.DBCon;
+
+public class BoardDAO {
+	
+	// 게시판 글 생성
+	public void createBoard() {
+		String sql = "INSERT INTO board\r\n"
+				+ "(\r\n"
+				+ "	NO,\r\n"
+				+ "	title,\r\n"
+				+ "	text,\r\n"
+				+ "	writer,\r\n"
+				+ "	firstdate\r\n"
+				+ ")\r\n"
+				+ "VALUES\r\n"
+				+ "(\r\n"
+				+ "	board_seq.NEXTVAL,\r\n"
+				+ "	?,\r\n" // 제목
+				+ "	?,\r\n" // 본문내용
+				+ "	?,\r\n" // 유저 번호
+				+ "	sysdate\r\n" // 날짜
+				+ ");";
+		
+		try (Connection con = DBCon.con(); PreparedStatement pstmt = con.prepareStatement(sql);) {
+			// 처리코드1
+			/*pstmt.setString(1, sch.getKind());
+			pstmt.setString(2, sch.getType());
+			pstmt.setString(3, "%"+sch.getName()+"%");*/
+
+			try (ResultSet rs = pstmt.executeQuery();) {
+				// 처리코드2
+				/*while (rs.next()) {
+					bevList.add(new Beverage(
+							rs.getInt("no"),
+							rs.getString("kind"),
+							rs.getString("type"),
+							rs.getString("name"),
+							rs.getInt("price")
+							));
+				}
+				System.out.println("건수:" + bevList.size());*/
+			}
+		} catch (SQLException e) {
+			System.out.println("DB 에러:" + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("일반 에러:" + e.getMessage());
+		}
+	}
+	
+}
---
+++ onecoffee/src/main/java/vo/Board.java
@@ -0,0 +1,79 @@
+package vo;
+
+import java.util.Date;
+
+public class Board {
+	private int no;
+	private String title;
+	private String text;
+	// 유저
+	private Date firstDate;
+	private Date modifyDate;
+	private boolean isEnd;
+	private boolean isNotice;
+	
+	
+	public Board(String title, String text, Date firstDate, boolean isNotice) {
+		this.title = title;
+		this.text = text;
+		this.firstDate = firstDate;
+		this.isNotice = isNotice;
+	}
+
+	public Board(int no, String title, String text, Date firstDate, Date modifyDate, boolean isEnd, boolean isNotice) {
+		this.no = no;
+		this.title = title;
+		this.text = text;
+		this.firstDate = firstDate;
+		this.modifyDate = modifyDate;
+		this.isEnd = isEnd;
+		this.isNotice = isNotice;
+	}
+
+	// 기본 getter, setter
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getFirstDate() {
+		return firstDate;
+	}
+	public void setFirstDate(Date firstDate) {
+		this.firstDate = firstDate;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public boolean isEnd() {
+		return isEnd;
+	}
+	public void setEnd(boolean isEnd) {
+		this.isEnd = isEnd;
+	}
+	public boolean isNotice() {
+		return isNotice;
+	}
+	public void setNotice(boolean isNotice) {
+		this.isNotice = isNotice;
+	}
+	
+	
+
+}
